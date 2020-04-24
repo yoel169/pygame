00abc2fa464fa37583147793ef1b515a71e8e601
yoel169 <yroba002@fiu.edu>
2020-04-23 20:18:12 -0400

    update 1.5

--- Game.py
+++ Game.py
@@ -1,16 +1,18 @@
-# Import the pygame module
+ # Import the pygame module
 import pygame
-import random
-import pygameMenu as pygame_menu
+
+from Players import (
+    Player,
+    Bullet,
+    Enemy,
+    Cloud
+)
+
+from Messages import points_update
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
 from pygame.locals import (
-    RLEACCEL,
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
     K_ESCAPE,
     K_SPACE,
     KEYDOWN,
@@ -31,116 +33,10 @@ pygame.mixer.music.load("game.mp3")
 pygame.mixer.music.play(loops=-1)
 
 # Load all sound files
-#move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
-#move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
+# move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
+# move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
 death_sound = pygame.mixer.Sound("BadHitSound.ogg")
 collision_sound = pygame.mixer.Sound("HitSound.ogg")
-
-# Define a player object by extending pygame.sprite.Sprite
-# The surface drawn on the screen is now an attribute of 'player'
-class Player(pygame.sprite.DirtySprite):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.image.load("jet.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect()
-        self._layer= 1
-        self.health = 15
-
-    # Move the sprite based on user keypresses
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-            #move_up_sound.play()
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-            #move_down_sound.play()
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
-
-        # Keep player on the screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-
-        if self.health <= 0:
-            self.kill()
-
-
-# Define the enemy object by extending pygame.sprite.Sprite
-# The surface you draw on the screen is now an attribute of 'enemy'
-class Enemy(pygame.sprite.DirtySprite):
-    def __init__(self):
-        super(Enemy, self).__init__()
-        self.surf = pygame.image.load("pf.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-        self._layer = 1
-        self.speed = random.randint(5, 20)
-        self.damage = 15
-        self.health = 15
-
-    # Move the sprite based on speed
-    # Remove the sprite when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-self.speed, 0)
-        if self.rect.right < 0:
-            self.kill()
-        elif self.health <= 0:
-            self.kill()
-            death_sound.play()
-
-
-class Cloud(pygame.sprite.DirtySprite):
-    def __init__(self):
-        super(Cloud, self).__init__()
-        self.surf = pygame.image.load("cloud.png").convert()
-        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        # The starting position is randomly generated
-        self._layer = 0
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-
-    # Move the cloud based on a constant speed
-    # Remove the cloud when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-5, 0)
-        if self.rect.right < 0:
-            self.kill()
-
-class Bullet(pygame.sprite.DirtySprite):
-    def __init__(self, position):
-        super(Bullet, self).__init__()
-        self.surf = pygame.image.load("carrot.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect( center = position)
-        self.pos = pygame.math.Vector2(position)
-        self.vel = pygame.math.Vector2(0, -450)
-        self._layer = 1
-        self.damage = 5
-
-    def update(self):
-        # Add the velocity to the position vector to move the sprite.
-        self.rect.move_ip(5, 0)
-        if self.rect.bottom <= 0 :
-            self.kill()
-
 
 # Initialize pygame
 pygame.init()
@@ -148,27 +44,6 @@ pygame.init()
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-def set_difficulty(value, difficulty):
-    todo = ' '# Do the job here !
-
-
-def start_the_game():
-    todo =''# Do the job here !
-
-
-menu = pygame_menu.Menu(height=300,
-                        width=400,
-                        theme=pygame_menu.themes.THEME_BLUE,
-                        title='Welcome')
-
-menu.add_text_input('Name :', default='John Doe')
-menu.add_selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add_button('Play', start_the_game)
-menu.add_button('Quit', pygame_menu.events.EXIT)
-
-menu.mainloop(screen)
 
 # Create a custom event for adding a new enemy/clouds
 ADDENEMY = pygame.USEREVENT + 1
@@ -193,6 +68,11 @@ running = True
 
 # Setup the clock for a decent framerate
 clock = pygame.time.Clock()
+start_time = 0
+
+score = 0
+
+pause = False
 
 # Main loop
 while running:
@@ -200,27 +80,30 @@ while running:
     for event in pygame.event.get():
         # Check for KEYDOWN event
         if event.type == KEYDOWN:
+            time_since_enter = pygame.time.get_ticks() - start_time
             # If the Esc key is pressed, then exit the main loop
-            if event.key == K_ESCAPE:
+            if (event.key == K_ESCAPE):
                 running = False
-            elif event.key == K_SPACE:
+            if event.key == K_SPACE and time_since_enter >= 300:
                 new_bullet = Bullet(player.rect.center)
                 bullets.add(new_bullet)
                 all_sprites.add(new_bullet)
+                start_time = pygame.time.get_ticks()
+                #print(str(start_time))
 
         # Check for QUIT event. If QUIT, then set running to false.
-        elif event.type == QUIT:
+        if event.type == QUIT:
             running = False
 
         # Add a new enemy?
-        elif event.type == ADDENEMY:
+        if event.type == ADDENEMY:
             # Create the new enemy and add it to sprite groups
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
 
         # Add a new cloud?
-        elif event.type == ADDCLOUD:
+        if event.type == ADDCLOUD:
             # Create the new cloud and add it to sprite groups
             new_cloud = Cloud()
             clouds.add(new_cloud)
@@ -250,6 +133,9 @@ while running:
         for bullet in bullet_list:
             enemy.health -= bullet.damage
             collision_sound.play()
+            if enemy.health <= 0:
+                  death_sound.play()
+                  score +=1
 
     # Check if any enemies have collided with the player
     if pygame.sprite.spritecollideany(player, enemies):
@@ -265,6 +151,8 @@ while running:
 
     # Ensure program maintains a rate of 30 frames per second
     clock.tick(30)
+
+    points_update(score, screen)
 
     # Update the display
     pygame.display.flip()
